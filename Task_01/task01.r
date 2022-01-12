5f9e84569a3784601f4b7c715851df04f8882278
klc0075 <klc0075@mix.wvu.edu>
2022-01-12 17:35:09 -0500

    Kayla Coffman Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,2 +1,29 @@
+#pw for git terminal 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
 #install.packages ("swirl")
 library (swirl)
+swirl ()
+Kayla Coffman
+1
+1
+5 + 7
+x <- 5 + 7
+x 
+y <- x - 3
+y
+z <- c(1.1, 9, 3.14)
+?c 
+z
+c(z, 555, z)
+z * 2 + 100
+my_sqrt <- sqrt(z - 1)
+2
+my_sqrt
+my_div <- z/my_sqrt
+3
+my_div
+c(1, 2, 3,4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+z * 2 + 1000
+my_div
+2
+klc0075@mix.wvu.edu
