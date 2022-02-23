f8cace54db864800d48ee6d0c9250e1b14b51e5d
klc0075 <klc0075@mix.wvu.edu>
2022-02-23 09:18:18 -0500

    Kayla Coffman Task 07

---
+++ Project/code/Butterfly Data Code .R
@@ -0,0 +1,4 @@
+setwd("/Users/kaylacoffman/Desktop/Evolution/Tasks/Project/data")
+#Data was on same excel but two different sheets 
+butterflyst <- read.csv('butterflystates.csv')
+butterflytt <- read.csv('butterflytraits.csv')
